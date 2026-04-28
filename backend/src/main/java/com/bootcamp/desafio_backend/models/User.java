0d76b8b329cfd5c5da215200af022abd47fec34b
Murilo Besson <murilo.besson@outlook.com>
2026-04-27 21:08:41 -0300

    feat: add User entity with JPA and Lombok mapping

---
+++ backend/src/main/java/com/bootcamp/desafio_backend/models/User.java
@@ -0,0 +1,49 @@
+package com.bootcamp.desafio_backend.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false, unique = true)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "cpf", nullable = false, unique = true)
+    private String cpf;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "avatar")
+    private String avatar;
+
+    @Column(name = "xp", nullable = false)
+    private Integer xp;
+
+    @Column(name = "level", nullable = false)
+    private Integer level;
+
+    @Column(name = "deletedAt")
+    private Date deletedAt;
+
+}
