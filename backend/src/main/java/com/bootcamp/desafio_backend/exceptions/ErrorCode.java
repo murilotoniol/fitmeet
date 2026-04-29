88f015ca9ba83cf2a2eeb7541cfb052a2132609a
Murilo Besson <murilo.besson@outlook.com>
2026-04-29 15:37:58 -0300

    feat: implement exception enum E1-E19

--- backend/src/main/java/com/bootcamp/desafio_backend/exceptions/ErrorCode.java
+++ backend/src/main/java/com/bootcamp/desafio_backend/exceptions/ErrorCode.java
@@ -1,4 +1,33 @@
 package com.bootcamp.desafio_backend.exceptions;
 
-public class ErrorCode {
+public enum ErrorCode {
+    E1("Informe os campos obrigatórios corretamente."),
+    E2("A imagem deve ser um arquivo PNG ou JPG."),
+    E3("O e-mail ou CPF informado já pertence a outro usuário."),
+    E4("Usuário não encontrado."),
+    E5("Senha incorreta."),
+    E6("Esta conta foi desativada e não pode ser utilizada."),
+    E7("Você já se registrou nesta atividade."),
+    E8("O criador da atividade não pode se inscrever como um participante."),
+    E9("Apenas participantes aprovados na atividade podem fazer check-in."),
+    E10("Código de confirmação incorreto."),
+    E11("Você já confirmou sua participação nesta atividade."),
+    E12("Não é possível se inscrever em uma atividade concluída."),
+    E13("Não é possível confirmar presença em uma atividade concluída."),
+    E14("Apenas o criador da atividade pode editá-la."),
+    E15("Apenas o criador da atividade pode exclui-la."),
+    E16("Apenas o criador da atividade pode aprovar ou negar participantes."),
+    E17("Apenas o criador da atividade pode concluí-la."),
+    E18("Não é possível cancelar sua inscrição, pois sua presença já foi confirmada."),
+    E19("Autenticação necessária.");
+
+    private final String message;
+
+    ErrorCode(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
