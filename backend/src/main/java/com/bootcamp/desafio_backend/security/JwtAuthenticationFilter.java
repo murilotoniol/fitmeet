642ea36600567e8b7d6b58905b5d113e4869a57f
Murilo Besson <murilo.besson@outlook.com>
2026-04-30 19:45:26 -0300

    feat: creates UserDetailsImpl and UserDetailsServiceImpl for security abstraction

---
+++ backend/src/main/java/com/bootcamp/desafio_backend/security/JwtAuthenticationFilter.java
@@ -0,0 +1,64 @@
+package com.bootcamp.desafio_backend.security;
+
+import com.bootcamp.desafio_backend.exceptions.BusinessException;
+import com.bootcamp.desafio_backend.exceptions.ErrorCode;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    public JwtAuthenticationFilter (JwtService jwtService, UserDetailsService userDetailsService) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authorizationHeader.replace("Bearer ", "");
+
+        String email = jwtService.extractSubject(token);
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+
+        if (!userDetails.isEnabled()) {
+            throw new BusinessException(ErrorCode.E6);
+        }
+
+        if (!jwtService.isTokenValid(token, ((UserDetailsImpl) userDetails).getUser())) {
+            throw new BusinessException(ErrorCode.E19);
+        }
+
+        UsernamePasswordAuthenticationToken authentication =
+                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ backend/src/main/java/com/bootcamp/desafio_backend/security/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.bootcamp.desafio_backend.security;
+
+import com.bootcamp.desafio_backend.exceptions.BusinessException;
+import com.bootcamp.desafio_backend.exceptions.ErrorCode;
+import com.bootcamp.desafio_backend.models.User;
+import com.bootcamp.desafio_backend.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new BusinessException(ErrorCode.E4));
+
+        return new UserDetailsImpl(user);
+    }
+}
