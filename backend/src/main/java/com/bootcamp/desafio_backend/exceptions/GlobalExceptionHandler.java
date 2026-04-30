dbcf5c36e28bcfee303c9a8f663b4f140f29f615
Murilo Besson <murilo.besson@outlook.com>
2026-04-30 14:23:44 -0300

    feat: implement validation exception for invalid/blank fields

--- backend/src/main/java/com/bootcamp/desafio_backend/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/com/bootcamp/desafio_backend/exceptions/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.bootcamp.desafio_backend.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import com.bootcamp.desafio_backend.dtos.response.ErrorResponse;
@@ -20,6 +21,22 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity
                 .status(exception.getErrorCode().getStatus())
+                .body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException exception) {
+
+        ErrorCode errorCode = ErrorCode.E1;
+
+        ErrorResponse response = new ErrorResponse(
+          errorCode.name(),
+          errorCode.getMessage(),
+          LocalDateTime.now()
+        );
+
+        return ResponseEntity
+                .status(errorCode.getStatus())
                 .body(response);
     }
 }
