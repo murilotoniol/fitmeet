cf8fbb07a9bf73af5ee8259042cbaa8829808ae6
Murilo Besson <murilo.besson@outlook.com>
2026-05-01 16:27:44 -0300

    feat: implements UserAchievementRepository.findByUserId

--- backend/src/main/java/com/bootcamp/desafio_backend/repositories/UserAchievementRepository.java
+++ backend/src/main/java/com/bootcamp/desafio_backend/repositories/UserAchievementRepository.java
@@ -4,8 +4,11 @@ import com.bootcamp.desafio_backend.models.UserAchievement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface UserAchievementRepository extends JpaRepository<UserAchievement, UUID> {
+
+    List<UserAchievement> findByUserId(UUID userId);
 }
