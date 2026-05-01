5bd12b27b995d685e8647d5a6d9f71acc82cb813
Murilo Besson <murilo.besson@outlook.com>
2026-05-01 17:08:06 -0300

    refactor(dto): change NotNull for NotEmpty in activityTypeIds

--- backend/src/main/java/com/bootcamp/desafio_backend/dtos/request/DefinePreferencesRequest.java
+++ backend/src/main/java/com/bootcamp/desafio_backend/dtos/request/DefinePreferencesRequest.java
@@ -1,12 +1,12 @@
 package com.bootcamp.desafio_backend.dtos.request;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 import java.util.UUID;
 
 public record DefinePreferencesRequest(
 
-        @NotNull(message = "A lista de preferências é obrigatória.")
+        @NotEmpty(message = "A lista de preferencias e obrigatoria.")
         List<UUID> activityTypeIds
 ) {
 }
