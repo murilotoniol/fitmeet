30e7313d47cfe4ab2a8d5e09867f3fdd81b36177
Murilo Besson <murilo.besson@outlook.com>
2026-05-01 16:20:16 -0300

    add wrapping in JwtAuthenticationFilter

--- backend/src/main/java/com/bootcamp/desafio_backend/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/bootcamp/desafio_backend/security/JwtAuthenticationFilter.java
@@ -13,6 +13,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 import java.io.IOException;
 
 @Component
@@ -20,10 +23,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
+    private final HandlerExceptionResolver handlerExceptionResolver;
 
-    public JwtAuthenticationFilter (JwtService jwtService, UserDetailsService userDetailsService) {
+    public JwtAuthenticationFilter (JwtService jwtService, UserDetailsService userDetailsService,
+                                    @Qualifier("handlerExceptionResolver") HandlerExceptionResolver handlerExceptionResolver) {
         this.jwtService = jwtService;
         this.userDetailsService = userDetailsService;
+        this.handlerExceptionResolver = handlerExceptionResolver;
     }
 
     @Override
@@ -33,32 +39,36 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             FilterChain filterChain
     ) throws ServletException, IOException {
 
-        String authorizationHeader = request.getHeader("Authorization");
+        try {
+            String authorizationHeader = request.getHeader("Authorization");
 
-        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            String token = authorizationHeader.replace("Bearer ", "");
+
+            String email = jwtService.extractSubject(token);
+
+            UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+
+            if (!userDetails.isEnabled()) {
+                throw new BusinessException(ErrorCode.E6);
+            }
+
+            if (!jwtService.isTokenValid(token, ((UserDetailsImpl) userDetails).getUser())) {
+                throw new BusinessException(ErrorCode.E19);
+            }
+
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
             filterChain.doFilter(request, response);
-            return;
+        } catch (Exception ex) {
+            handlerExceptionResolver.resolveException(request, response, null, ex);
         }
-
-        String token = authorizationHeader.replace("Bearer ", "");
-
-        String email = jwtService.extractSubject(token);
-
-        UserDetails userDetails = userDetailsService.loadUserByUsername(email);
-
-        if (!userDetails.isEnabled()) {
-            throw new BusinessException(ErrorCode.E6);
-        }
-
-        if (!jwtService.isTokenValid(token, ((UserDetailsImpl) userDetails).getUser())) {
-            throw new BusinessException(ErrorCode.E19);
-        }
-
-        UsernamePasswordAuthenticationToken authentication =
-                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        filterChain.doFilter(request, response);
     }
 }
