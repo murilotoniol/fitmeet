c0e4fb5f62738231373330ea4d459e8cbb09d065
Murilo Besson <murilo.besson@outlook.com>
2026-05-02 22:41:42 -0300

    feat: implement search by name in AchievementRepository

--- backend/src/main/java/com/bootcamp/desafio_backend/repositories/AchievementRepository.java
+++ backend/src/main/java/com/bootcamp/desafio_backend/repositories/AchievementRepository.java
@@ -4,8 +4,11 @@ import com.bootcamp.desafio_backend.models.Achievement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AchievementRepository extends JpaRepository<Achievement, UUID> {
+
+    Optional<Achievement> findByName(String name);
 }
