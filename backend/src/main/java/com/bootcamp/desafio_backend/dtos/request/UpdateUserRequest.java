87d2739fe73203bdf38c54bfa1cfcc6daa943f9f
Murilo Besson <murilo.besson@outlook.com>
2026-05-02 22:53:28 -0300

    feat: add option to change password in UpdateUserRequest

--- backend/src/main/java/com/bootcamp/desafio_backend/dtos/request/UpdateUserRequest.java
+++ backend/src/main/java/com/bootcamp/desafio_backend/dtos/request/UpdateUserRequest.java
@@ -2,14 +2,18 @@ package com.bootcamp.desafio_backend.dtos.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record UpdateUserRequest(
 
-        @NotBlank(message = "Informe os campos obrigatórios corretamente.")
+        @NotBlank(message = "Informe os campos obrigatorios corretamente.")
         String name,
 
-        @NotBlank(message = "Informe os campos obrigatórios corretamente.")
-        @Email(message = "O formato do e-mail é inválido.")
-        String email
+        @NotBlank(message = "Informe os campos obrigatorios corretamente.")
+        @Email(message = "O formato do e-mail e invalido.")
+        String email,
+
+        @Size(min = 6, message = "A senha deve ter pelo menos 6 caracteres.")
+        String password
 ) {
 }
