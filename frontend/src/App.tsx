6381ad03bf6df93d00d87c7566830604993eb18e
Murilo Besson <murilo.besson@outlook.com>
2026-05-13 12:48:37 -0300

    refactor: clear App.tsx and implement routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,8 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 
-function LoginPage() {
-  return <main>Login</main>;
-}
-
-function RegisterPage() {
-  return <main>Cadastro</main>;
-}
-
-function HomePage() {
-  return <main>Home</main>;
-}
-
-function ActivitiesPage() {
-  return <main>Atividades</main>;
-}
-
-function NewActivityPage() {
-  return <main>Nova atividade</main>;
-}
-
-function ProfilePage() {
-  return <main>Perfil</main>;
-}
+import { LoginPage } from "./features/auth/login-page";
+import { RegisterPage } from "./features/auth/register-page";
+import { PreferencesPage } from "./features/preferences/preferences-page";
 
 function App() {
   return (
@@ -31,10 +11,11 @@ function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/cadastro" element={<RegisterPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/atividades" element={<ActivitiesPage />} />
-        <Route path="/atividades/nova" element={<NewActivityPage />} />
-        <Route path="/perfil" element={<ProfilePage />} />
+        <Route path="/home" element={<PreferencesPage />} />
+        <Route path="/preferencias" element={<PreferencesPage />} />
+        <Route path="/atividades" element={<PreferencesPage />} />
+        <Route path="/atividades/nova" element={<PreferencesPage />} />
+        <Route path="/perfil" element={<PreferencesPage />} />
       </Routes>
     </BrowserRouter>
   );
