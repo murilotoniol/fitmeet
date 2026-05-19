5d0695f75be7659d6696680a6c03a3d38772ad7b
Murilo Besson <murilo.besson@outlook.com>
2026-05-19 08:33:41 -0300

    chore(frontend): configure react app foundation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 
+import { ActivityDetailsPage } from "./features/activities/activity-details-page";
+import { ActivityEditPage } from "./features/activities/activity-edit-page";
+import { ActivityTypePage } from "./features/activities/activity-type-page";
+import { NewActivityPage } from "./features/activities/new-activity-page";
 import { LoginPage } from "./features/auth/login-page";
 import { RegisterPage } from "./features/auth/register-page";
+import { HomePage } from "./features/home/home-page";
 import { PreferencesPage } from "./features/preferences/preferences-page";
+import { EditProfilePage } from "./features/profile/edit-profile-page";
+import { ProfilePage } from "./features/profile/profile-page";
+import { ProtectedRoute } from "./routes/protected-route";
 
 function App() {
   return (
@@ -11,11 +19,18 @@ function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/cadastro" element={<RegisterPage />} />
-        <Route path="/home" element={<PreferencesPage />} />
-        <Route path="/preferencias" element={<PreferencesPage />} />
-        <Route path="/atividades" element={<PreferencesPage />} />
-        <Route path="/atividades/nova" element={<PreferencesPage />} />
-        <Route path="/perfil" element={<PreferencesPage />} />
+        <Route path="/home" element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
+        <Route path="/preferencias" element={<ProtectedRoute><PreferencesPage /></ProtectedRoute>} />
+        <Route path="/atividades" element={<ProtectedRoute><ActivityTypePage /></ProtectedRoute>} />
+        <Route path="/atividades/detalhes/:activityId" element={<ProtectedRoute><ActivityDetailsPage /></ProtectedRoute>} />
+        <Route path="/atividades/editar/:activityId" element={<ProtectedRoute><ActivityEditPage /></ProtectedRoute>} />
+        <Route path="/atividades/nova" element={<ProtectedRoute><NewActivityPage /></ProtectedRoute>} />
+        <Route path="/perfil" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
+        <Route path="/perfil/editar" element={<ProtectedRoute><EditProfilePage /></ProtectedRoute>} />
+        <Route
+          path="/perfil/editar/desativar"
+          element={<ProtectedRoute><EditProfilePage showDeactivateDialog /></ProtectedRoute>}
+        />
       </Routes>
     </BrowserRouter>
   );
