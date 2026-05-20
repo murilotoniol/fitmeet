e1215d6fb7082b9ac753fbfa73bbcf3961dcd681
Murilo Besson <murilo.besson@outlook.com>
2026-05-19 23:36:56 -0300

    fix(backend): configure application timezone

--- backend/src/main/java/com/bootcamp/desafio_backend/DesafioBackendApplication.java
+++ backend/src/main/java/com/bootcamp/desafio_backend/DesafioBackendApplication.java
@@ -3,10 +3,15 @@ package com.bootcamp.desafio_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class DesafioBackendApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone(
+				System.getenv().getOrDefault("APP_TIME_ZONE", "America/Sao_Paulo")
+		));
 		SpringApplication.run(DesafioBackendApplication.class, args);
 	}
 
